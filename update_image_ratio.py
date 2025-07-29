c784ee1afcbcc70cacb7e0c09ac016623fb7ed02
MagicTheDev <matthewanderson3607@gmail.com>
2025-07-29 14:31:30 -0500

    feat: new assets

--- update_image_ratio.py
+++ update_image_ratio.py
@@ -1,7 +1,7 @@
 import os, glob, re
 from PIL import Image
 
-NEW_NAME   = "blacksmith"
+NEW_NAME   = "crafting-station"
 INPUT_DIR  = f"assets/home-base/buildings/{NEW_NAME}"
 OUTPUT_DIR = INPUT_DIR
 DELETE = True
