71dbc0765e0a1a60f0f5649edb77f2331298b76d
MagicTheDev <matthewanderson3607@gmail.com>
2025-07-29 11:41:14 -0500

    feat: new assets

--- update_image_ratio.py
+++ update_image_ratio.py
@@ -1,7 +1,7 @@
 import os, glob, re
 from PIL import Image
 
-NEW_NAME   = "army-camp"
+NEW_NAME   = "air-bombs"
 INPUT_DIR  = f"assets/builder-base/buildings/{NEW_NAME}"
 OUTPUT_DIR = INPUT_DIR
 DELETE = True
