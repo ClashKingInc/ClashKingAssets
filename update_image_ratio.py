f4136c6c4bd25ea22fd8b0d48f33ddf93d737ca3
MagicTheDev <matthewanderson3607@gmail.com>
2025-07-29 14:02:48 -0500

    feat: new assets

--- update_image_ratio.py
+++ update_image_ratio.py
@@ -1,8 +1,8 @@
 import os, glob, re
 from PIL import Image
 
-NEW_NAME   = "ottos-outpost"
-INPUT_DIR  = f"assets/builder-base/buildings/{NEW_NAME}"
+NEW_NAME   = "blacksmith"
+INPUT_DIR  = f"assets/home-base/buildings/{NEW_NAME}"
 OUTPUT_DIR = INPUT_DIR
 DELETE = True
 
