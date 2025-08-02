83501801d621e6a163a8ec5efb8e40d71b4e55a4
MagicTheDev <matthewanderson3607@gmail.com>
2025-08-01 20:53:39 -0500

    feat: new assets

--- update_image_ratio.py
+++ update_image_ratio.py
@@ -1,7 +1,7 @@
 import os, glob, re
 from PIL import Image
 
-NEW_NAME   = "siege-barracks"
+NEW_NAME   = "super-minion"
 INPUT_DIR  = f"assets/home-base/troops/{NEW_NAME}"
 OUTPUT_DIR = INPUT_DIR
 
